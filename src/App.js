3978ea826e9343122141f5edf93ebf6684e6dbc1
Fotis Tsakiris <footios76@gmail.com>
2019-04-15 06:16:17 +0300

    Layout and Builder components added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout';
+import Builder from './containers/Builder';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Layout>
+					<Builder />
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/containers/Builder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import Aux from '../hoc/Aux'
+
+class Builder extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Aux>
+                Builder
+            </Aux>
+         );
+    }
+}
+ 
+export default Builder;
