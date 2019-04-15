a3b2f41b4722952ae38af25e7ea096606b1f15ab
Fotis Tsakiris <footios76@gmail.com>
2019-04-15 12:48:47 +0300

    159. BuildControls added

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import BuildControl from "./BuildControl/BuildControl";
+
+import classes from './BuildControls.module.css'
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+
+const buildControls = props => {
+
+  // let button = 'START ADDING INGREDIENTS'
+  // if (props.isAuth) {
+  //   button = 'ORDER NOW'
+  // } else if (props.building && !props.isAuth) {
+  //   button = 'BURGER READY? SIGN UP AND ORDER'
+  // }
+  return (
+    <div className={classes.BuildControls}>
+    {/* <p>
+      Current Price: <strong>{props.price.toFixed(2)}</strong>
+    </p> */}
+    {controls.map(ctrl => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]} // we access e.g. {salad: true, meat: false}
+      />
+    ))}
+    <button
+      className={classes.OrderButton}
+      // disabled={!props.purchasable}
+      // onClick={props.ordered}
+    >
+    {/* {button} */}
+    ORDER
+      
+    </button>
+  </div>
+  )
+}
+  
+
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,32 @@
 import React from 'react';
 // import { withRouter } from 'react-router-dom'
+import uuidv4 from 'uuid/v4';
 
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 // child of BurgerBuilder
 const burger = ({ ingredients }) => {
-	let transformedIngredients = Object.keys(ingredients).map((igKey, index) => (
-		<BurgerIngredient key={igKey + index} type={igKey} />
-	));
+	let transformedIngredients = Object.keys(ingredients)
+	.map(igKey => {
+		// transform the igKey, string value, into an array
+		// with as many elements as the value of the igKey
+		// We use an underscore in the second map func,
+		// because we don't care about the element itself,
+		// since we are going to pass it to the <BurgerIngredient />
+		// through 'type'.
+		// console.log('igKey', igKey)
+		return [...Array(ingredients[igKey])].map((_, index) => {
+			// console.log(ingredients[igKey])
+			return <BurgerIngredient key={uuidv4()} type={igKey} />
+
+		}
+		);
+	})
+
+		.reduce((arr, el) => arr.concat(el), []);
+
+	// console.log(transformedIngredients);
 
 	if (transformedIngredients.length === 0) {
 		transformedIngredients = <p> Please start adding ingredients! </p>;
